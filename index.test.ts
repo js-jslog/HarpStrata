eb71b50304bbb2ddf99009525979445b1023fd70
js-jslog <jhs4jbs@hotmail.co.uk>
2020-04-04 21:52:31 +0000

    Add empty tests for getLayouts function

--- index.test.ts
+++ index.test.ts
@@ -1,6 +1,10 @@
-import i from './index';
+import getLayouts from './index';
 
-test('index contains an index function', () => {
-  expect(i).toBeDefined();
+test('index contains a getLayouts function', () => {
+  expect(getLayouts).toBeDefined();
+});
+
+test('getLayouts function returns an array', () => {
+  expect(Array.isArray(getLayouts())).toBeTruthy();
 });
 
--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
-const index2 = (input: string) => input;
+const getLayouts = () => [];
 
-// module.exports = index2(1); // Uncomment to demonstrate that ts-jest performs type checking
-export default index2('1');
+export default getLayouts;
 
