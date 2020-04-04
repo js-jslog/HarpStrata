fe01e8d59c16ccc8e175a790afc171c301606e6b
js-jslog <jhs4jbs@hotmail.co.uk>
2020-04-04 22:49:45 +0000

    Refactor so that matrix and layout are co-sourced

--- index.ts
+++ index.ts
@@ -1,23 +1,26 @@
-const getLayouts = () => ['major-diatonic'];
-const getHarpMatrix = () => {
-  const matrix: [
-    'b3', undefined, undefined, 'b3',
-    '1', '3', '5', '1',
-    '2', '5', '7', '2',
-    'b2', 'b5', 'b7', 'b2',
-    undefined, '4', '6', undefined,
-    undefined, undefined, 'b6', undefined,
-  ] = [
-    'b3',     ,     , 'b3',
-    '1' , '3' , '5' , '1' ,
-    '2' , '5' , '7' , '2' ,
-    'b2', 'b5', 'b7', 'b2',
-        , '4' , '6' ,     ,
-        ,     , 'b6',     ,
-  ];
+const majorDiatonicLayout: [
+  'b3', undefined, undefined, 'b3',
+  '1', '3', '5', '1',
+  '2', '5', '7', '2',
+  'b2', 'b5', 'b7', 'b2',
+  undefined, '4', '6', undefined,
+  undefined, undefined, 'b6', undefined,
+] = [
+  'b3',     ,     , 'b3',
+  '1' , '3' , '5' , '1' ,
+  '2' , '5' , '7' , '2' ,
+  'b2', 'b5', 'b7', 'b2',
+      , '4' , '6' ,     ,
+      ,     , 'b6',     ,
+];
 
-  return matrix;
+const layoutMap = {
+  'major-diatonic': majorDiatonicLayout,
 };
+
+const getLayouts = () => Object.keys(layoutMap);
+
+const getHarpMatrix = () => layoutMap['major-diatonic'];
 
 export { getLayouts, getHarpMatrix };
 
