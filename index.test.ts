8576d7348dfd6329a330c593143ea18c43a05958
js-jslog <jhs4jbs@hotmail.co.uk>
2020-04-04 22:38:26 +0000

    Add first 4 holes of the major diatonic layout

--- index.test.ts
+++ index.test.ts
@@ -12,3 +12,23 @@ test('getLayouts function returns an array with \'major-diatonic\' as one of the
   expect(getLayouts().includes('major-diatonic')).toBeTruthy();
 });
 
+test('getHarpMatrix function can return a first position major diatonic matrix', () => {
+  const expectedMatrix: [
+    'b3', undefined, undefined, 'b3',
+    '1', '3', '5', '1',
+    '2', '5', '7', '2',
+    'b2', 'b5', 'b7', 'b2',
+    undefined, '4', '6', undefined,
+    undefined, undefined, 'b6', undefined,
+  ] = [
+    'b3',     ,     , 'b3',
+    '1' , '3' , '5' , '1' ,
+    '2' , '5' , '7' , '2' ,
+    'b2', 'b5', 'b7', 'b2',
+        , '4' , '6' ,     ,
+        ,     , 'b6',     ,
+  ];
+  const actualMatrix = getHarpMatrix();
+
+  expect(actualMatrix).toStrictEqual(expectedMatrix);
+});
--- index.ts
+++ index.ts
@@ -1,5 +1,23 @@
 const getLayouts = () => ['major-diatonic'];
-const getHarpMatrix = () => {};
+const getHarpMatrix = () => {
+  const matrix: [
+    'b3', undefined, undefined, 'b3',
+    '1', '3', '5', '1',
+    '2', '5', '7', '2',
+    'b2', 'b5', 'b7', 'b2',
+    undefined, '4', '6', undefined,
+    undefined, undefined, 'b6', undefined,
+  ] = [
+    'b3',     ,     , 'b3',
+    '1' , '3' , '5' , '1' ,
+    '2' , '5' , '7' , '2' ,
+    'b2', 'b5', 'b7', 'b2',
+        , '4' , '6' ,     ,
+        ,     , 'b6',     ,
+  ];
+
+  return matrix;
+};
 
 export { getLayouts, getHarpMatrix };
 
