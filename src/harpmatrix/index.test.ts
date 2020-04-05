42fa739939954cb7aa551a33ed8806a0b19f5652
js-jslog <jhs4jbs@hotmail.co.uk>
2020-04-05 21:45:09 +0000

    Remove redundant tests

--- src/harpmatrix/index.test.ts
+++ src/harpmatrix/index.test.ts
@@ -4,14 +4,6 @@ import {
   example_majorDiatonicLayout_secondPosition,
 } from './testResources';
 
-test('index contains a getLayouts function', () => {
-  expect(getLayouts).toBeDefined();
-});
-
-test('getLayouts function returns an array', () => {
-  expect(Array.isArray(getLayouts())).toBeTruthy();
-});
-
 test('getLayouts function returns an array with \'major-diatonic\' as one of the values', () => {
   expect(getLayouts().includes('major-diatonic')).toBeTruthy();
 });
