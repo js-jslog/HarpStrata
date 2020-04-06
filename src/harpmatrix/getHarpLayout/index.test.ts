3bb38ec9c9a1970517356f2a2492efc26f1640c8
js-jslog <jhs4jbs@hotmail.co.uk>
2020-04-06 20:27:45 +0000

    Add a Layouts enumerator

--- src/harpmatrix/getHarpLayout/index.test.ts
+++ src/harpmatrix/getHarpLayout/index.test.ts
@@ -1,9 +1,10 @@
 import { example_majorDiatonicLayout } from './testResources';
+import { Layouts } from './types'
 import getHarpLayout from './index';
 
 test('getHarpLayout function can return a major diatonic layout', () => {
   const expectedMatrix = example_majorDiatonicLayout;
-  const actualMatrix = getHarpLayout();
+  const actualMatrix = getHarpLayout(Layouts.MajorDiatonic);
 
   expect(actualMatrix).toStrictEqual(expectedMatrix);
 });
--- src/harpmatrix/getHarpLayout/index.ts
+++ src/harpmatrix/getHarpLayout/index.ts
@@ -1,3 +1,4 @@
+import { Layouts } from './types';
 import MajorDiatonicLayout from './MajorDiatonicLayout';
 
-export default () => MajorDiatonicLayout;
+export default (layout: Layouts) => MajorDiatonicLayout;
