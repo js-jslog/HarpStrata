1f29e10f822692eca8302faec77a695489b627ae
js-jslog <jhs4jbs@hotmail.co.uk>
2020-04-07 20:08:36 +0000

    Previous commit was actually missing the new test

--- src/harpmatrix/getDegreeMatrix/index.test.ts
+++ src/harpmatrix/getDegreeMatrix/index.test.ts
@@ -9,7 +9,7 @@ test('getDegreeMatrix function maps a simple 2d array of 0\'s to root degrees wh
 
 test('getDegreeMatrix function maps a simple 2d array of 7\'s to root degrees when halfsetp offset is 7', () => {
   const expectedArray = [[ 1, ], [ 1, ]];
-  const actualArray = getDegreeMatrix([[ 0, ], [ 0, ]], 0);
+  const actualArray = getDegreeMatrix([[ 7, ], [ 7, ]], 7);
 
   expect(actualArray).toStrictEqual(expectedArray);
 });
