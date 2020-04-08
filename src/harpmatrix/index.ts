6bc977f8ed20c2a0b6f60f6fc85e38ac003858d4
js-jslog <jhs4jbs@hotmail.co.uk>
2020-04-08 20:19:20 +0000

    Remove unused layout variables

--- src/harpmatrix/index.ts
+++ src/harpmatrix/index.ts
@@ -3,28 +3,6 @@ import { getHarpLayouts, getHarpLayout } from './HarpLayout'
 import { Layouts } from './HarpLayout/types'
 import { Harpmatrix } from '../common/types'
 
-const majorDiatonicLayout: [
-  [ 'b3', undefined, undefined, 'b3', ],
-  [ '1', '3', '5', '1', ],
-  [ '2', '5', '7', '2', ],
-  [ 'b2', 'b5', 'b7', 'b2', ],
-  [ undefined, '4', '6', undefined, ],
-  [ undefined, undefined, 'b6', undefined, ],
-] = [
-  [ 'b3',     ,     , 'b3', ],
-  [ '1' , '3' , '5' , '1' , ],
-  [ '2' , '5' , '7' , '2' , ],
-  [ 'b2', 'b5', 'b7', 'b2', ],
-  [     , '4' , '6' ,     , ],
-  [     ,     , 'b6',     , ],
-];
-
-const layoutMap: {
-  'major-diatonic': Harpmatrix;
-} = {
-  'major-diatonic': majorDiatonicLayout,
-};
-
 const getLayouts = getHarpLayouts;
 
 enum Positions {
