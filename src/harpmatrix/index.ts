4d1666cd3b765eb028da7bc1827e5509635bb763
js-jslog <jhs4jbs@hotmail.co.uk>
2020-04-08 20:22:05 +0000

    Reorder variables in file for readibility

--- src/harpmatrix/index.ts
+++ src/harpmatrix/index.ts
@@ -5,26 +5,25 @@ import { Harpmatrix } from '../common/types'
 
 const getLayouts = getHarpLayouts;
 
+const getHarpmatrix = (layout: Layouts, position: string) => {
+  const matrix = getHarpLayout(layout).halfstepmatrix;
+
+  return getDegreeMatrix(matrix, positionMap[position]);
+};
+
 enum Positions {
   First = 'FIRST',
   Second = 'SECOND',
+};
+const positionMap = {
+  first: 0,
+  second: 7,
 };
 const getPositions = (layout: string): Positions[] => [
   Positions.First,
   Positions.Second,
 ];
 
-const getHarpmatrix = (layout: Layouts, position: string) => {
-  const matrix = getHarpLayout(layout).halfstepmatrix;
-
-  const positionMap = {
-    first: 0,
-    second: 7,
-  };
-
-  return getDegreeMatrix(matrix, positionMap[position]);
-
-};
 
 export {
   getLayouts,
