2df37b7f81c1e4a6591b3fd5a3b161e72d23549e
js-jslog <jhs4jbs@hotmail.co.uk>
2020-04-09 22:19:31 +0000

    Set constants for interaction types

--- src/harpmatrix/HarpLayout/Interaction/index.ts
+++ src/harpmatrix/HarpLayout/Interaction/index.ts
@@ -1,8 +1,8 @@
-import { InteractionIds, Interaction } from './types';
+import { InteractionIds } from './types';
 
-export const overblow1: Interaction = { id: InteractionIds.Overblow1 };
-export const blow: Interaction = { id: InteractionIds.Blow };
-export const draw: Interaction = { id: InteractionIds.Draw };
-export const bend1: Interaction = { id: InteractionIds.Bend1 };
-export const bend2: Interaction = { id: InteractionIds.Bend2 };
-export const bend3: Interaction = { id: InteractionIds.Bend3 };
+export const overblow1: { id: InteractionIds.Overblow1 } = { id: InteractionIds.Overblow1 };
+export const blow: { id: InteractionIds.Blow } = { id: InteractionIds.Blow };
+export const draw: { id: InteractionIds.Draw } = { id: InteractionIds.Draw };
+export const bend1: { id: InteractionIds.Bend1 } = { id: InteractionIds.Bend1 };
+export const bend2: { id: InteractionIds.Bend2 } = { id: InteractionIds.Bend2 };
+export const bend3: { id: InteractionIds.Bend3 } = { id: InteractionIds.Bend3 };
