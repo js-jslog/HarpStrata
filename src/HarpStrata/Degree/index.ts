85fd9d58b9203a4f071a30aa30c5de549fd2caff
js-jslog <jhs4jbs@hotmail.co.uk>
2020-04-13 21:08:27 +0000

    Improve shiftedDegreeIds construction

--- src/HarpStrata/Degree/index.ts
+++ src/HarpStrata/Degree/index.ts
@@ -5,14 +5,7 @@ import { ORDERED_DEGREES } from './constants'
 
 
 export const getDegreeMatrix = (halfstepIndexMatrix: HalfstepIndexMatrix, root: HalfstepIndex): DegreeMatrix => {
-  const shiftedDegreeIds = [ ...ORDERED_DEGREES ]
-
-  for (let i = 0; i < root; i++) {
-    // TODO: there must be a better way to achieve this
-    // const shiftedDegreeIds = a.slice(-1 * root).concat(a.slice(0, (-1 * root)))
-    const poppedDegreeIds = shiftedDegreeIds.pop()
-    if (poppedDegreeIds) shiftedDegreeIds.unshift(poppedDegreeIds)
-  }
+  const shiftedDegreeIds = ORDERED_DEGREES.slice(-1 * root).concat(ORDERED_DEGREES.slice(0, (-1 * root)))
 
   return halfstepIndexMatrix.map((row) => {
     return row.map((element) => {
