d6f8cb46dd6ae96f432682bdf7782f637dd9a3d6
js-jslog <jhs4jbs@hotmail.co.uk>
2020-04-13 20:13:16 +0000

    Fix typescript errors

--- src/HarpStrata/Degree/index.ts
+++ src/HarpStrata/Degree/index.ts
@@ -1,17 +1,21 @@
+import { HalfstepIndexMatrix, HalfstepIndex } from '../Apparatus/HalfstepIndex/types'
+
 import { DegreeMatrix } from './types'
 import { ORDERED_DEGREES } from './constants'
 
 
-export const getDegreeMatrix = (halfstepIndexMatrix, offset): DegreeMatrix => {
+export const getDegreeMatrix = (halfstepIndexMatrix: HalfstepIndexMatrix, root: HalfstepIndex): DegreeMatrix => {
   const shiftedDegreeIds = [ ...ORDERED_DEGREES ]
 
-  for (let i = 0; i < offset; i++) {
-    shiftedDegreeIds.unshift(shiftedDegreeIds.pop())
+  for (let i = 0; i < root; i++) {
+    // TODO: there must be a better way to achieve this
+    const poppedDegreeIds = shiftedDegreeIds.pop()
+    if (poppedDegreeIds) shiftedDegreeIds.unshift(poppedDegreeIds)
   }
 
   return halfstepIndexMatrix.map((row) => {
     return row.map((element) => {
-      return shiftedDegreeIds[element % 12]
+      return (element ? shiftedDegreeIds[element % 12] : undefined)
     })
   })
 }
