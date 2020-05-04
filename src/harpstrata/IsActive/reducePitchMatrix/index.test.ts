ebe269eb069a0f403dad0c38ce569b4a024d21cb
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-04 16:14:31 +0000

    Add reducer for PitchMatrices built on top of one for PitchRows

--- src/harpstrata/IsActive/reducePitchMatrix/index.test.ts
+++ src/harpstrata/IsActive/reducePitchMatrix/index.test.ts
@@ -1,18 +1,39 @@
-import { PitchIds, C, D, E, F } from '../../Pitch'
-import { DegreeIds, ROOT, SECOND, THIRD, FOURTH } from '../../Degree'
+import { PitchIds, C, D, E, F, G, A, B } from '../../Pitch'
+import type { PitchMatrix, PitchRow } from '../../Pitch'
+import { DegreeIds, ROOT, SECOND, THIRD, FOURTH, FIFTH, SIXTH, SEVENTH } from '../../Degree'
+import type { DegreeMatrix, DegreeRow } from '../../Degree'
 
-import type { ReducedState } from './index'
-import { reducePitchRow } from './index'
+import type { ReducedRowState, ReducedMatrixState } from './index'
+import { reducePitchRow, reducePitchMatrix } from './index'
 
-test('reducePitchRow will take a row of pitches and create an array of equivalent Degrees', () => {
-  const degreeRow = [ ROOT , SECOND, THIRD, FOURTH ]
-  const pitchRow = [ C, D, E, F ]
+test('reducePitchRow will take a list of active pitches and create an array of equivalent Degrees given an paired Pitch and Degree row', () => {
+  const degreeRow: DegreeRow = [ ROOT , SECOND, THIRD, FOURTH ]
+  const pitchRow: PitchRow = [ C, D, E, F ]
   const activePitchIds = [ PitchIds.D, PitchIds.F ]
   const expectedDegreeIds: DegreeIds[] = [ DegreeIds.Second, DegreeIds.Fourth ]
 
-  const initialState: ReducedState = { degreeRow, activePitchIds, activeDegreeIds: [] }
+  const initialState: ReducedRowState = { degreeRow, activePitchIds, activeDegreeIds: [] }
 
   const { activeDegreeIds } = pitchRow.reduce(reducePitchRow, initialState)
+
+  expect(activeDegreeIds).toStrictEqual(expectedDegreeIds)
+})
+
+test('reducePitchMatrix will take a list of active pitches and create an array of equivalent Degrees given an paired Pitch and Degree row', () => {
+  const degreeMatrix: DegreeMatrix = [
+    [ ROOT , SECOND, THIRD, FOURTH ],
+    [ FIFTH, SIXTH, SEVENTH, ROOT  ],
+  ]
+  const pitchMatrix: PitchMatrix = [
+    [ C, D, E, F ],
+    [ G, A, B, C ],
+  ]
+  const activePitchIds = [ PitchIds.D, PitchIds.F, PitchIds.G ]
+  const expectedDegreeIds: DegreeIds[] = [ DegreeIds.Second, DegreeIds.Fourth, DegreeIds.Fifth ]
+
+  const initialState: ReducedMatrixState = { degreeMatrix, activePitchIds, activeDegreeIds: [] }
+
+  const { activeDegreeIds } = pitchMatrix.reduce(reducePitchMatrix, initialState)
 
   expect(activeDegreeIds).toStrictEqual(expectedDegreeIds)
 })
--- src/harpstrata/IsActive/reducePitchMatrix/index.ts
+++ src/harpstrata/IsActive/reducePitchMatrix/index.ts
@@ -1,13 +1,18 @@
-import type { Pitch, PitchIds } from '../../Pitch'
-import type { Degree, DegreeIds } from '../../Degree'
+import type { Pitch, PitchRow, PitchIds } from '../../Pitch'
+import type { DegreeRow, DegreeMatrix, DegreeIds } from '../../Degree'
 
-export type ReducedState = {
-  readonly degreeRow: Degree[];
-  readonly activePitchIds: PitchIds[];
-  readonly activeDegreeIds: DegreeIds[];
+export type ReducedRowState = {
+  readonly degreeRow: DegreeRow;
+  readonly activePitchIds: ReadonlyArray<PitchIds>;
+  readonly activeDegreeIds: ReadonlyArray<DegreeIds>;
+}
+export type ReducedMatrixState = {
+  readonly degreeMatrix: DegreeMatrix;
+  readonly activePitchIds: ReadonlyArray<PitchIds>;
+  readonly activeDegreeIds: ReadonlyArray<DegreeIds>;
 }
 
-export const reducePitchRow = (reducedState: ReducedState, nextPitch: Pitch | undefined): ReducedState => {
+export const reducePitchRow = (reducedState: ReducedRowState, nextPitch: Pitch | undefined): ReducedRowState => {
   const { degreeRow, activePitchIds, activeDegreeIds } = reducedState
   const [ thisDegree, ...remainingDegreeRow ] = degreeRow
 
@@ -16,4 +21,23 @@ export const reducePitchRow = (reducedState: ReducedState, nextPitch: Pitch | un
   if (!activePitchIds.includes(nextPitch.id)) return { ...reducedState, degreeRow: remainingDegreeRow }
 
   return { ...reducedState, degreeRow: remainingDegreeRow, activeDegreeIds: [ ...activeDegreeIds, thisDegree.id ]}
+}
+
+export const reducePitchMatrix = (reducedState: ReducedMatrixState, nextPitchRow: PitchRow): ReducedMatrixState => {
+  const { degreeMatrix, activePitchIds, activeDegreeIds } = reducedState
+  const [ thisDegreeRow, ...remainingDegreeMatrix ] = degreeMatrix
+
+  const initialState: ReducedRowState = {
+    activePitchIds, activeDegreeIds, degreeRow: thisDegreeRow
+  }
+
+  const reducedRow: ReducedRowState = nextPitchRow.reduce(reducePitchRow, initialState)
+
+  const reducedMatrix: ReducedMatrixState = {
+    degreeMatrix: remainingDegreeMatrix,
+    activePitchIds: reducedRow.activePitchIds,
+    activeDegreeIds: reducedRow.activeDegreeIds,
+  }
+
+  return reducedMatrix
 }
