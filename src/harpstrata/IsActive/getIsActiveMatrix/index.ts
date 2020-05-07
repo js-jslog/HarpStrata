e1acde5a049707e3bd8e26ef7c522e874edf8ba7
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-07 16:05:20 +0000

    Followup to previous commit
    This change should have been made in the previous commit.

--- src/harpstrata/IsActive/getIsActiveMatrix/index.ts
+++ src/harpstrata/IsActive/getIsActiveMatrix/index.ts
@@ -1,12 +1,12 @@
 import type { IsActiveProps, IsActiveMatrix } from '../types'
 import { DegreeIds } from '../../Degree'
 
-import { getIsActiveMatrixForDegrees, getIsActiveMatrixForPitches } from './getIsActiveMatrixForSpecificType'
+import { getMatrixGivenDegree, getMatrixGivenPitch } from './getIsActiveMatrixForSpecificType'
 
 export const getIsActiveMatrix = (props: IsActiveProps): IsActiveMatrix => {
   const { activeElementIds } = props
   if (activeElementIds[0] in Object.values(DegreeIds)) {
-    return getIsActiveMatrixForDegrees(props)
+    return getMatrixGivenDegree(props)
   }
-  return getIsActiveMatrixForPitches(props)
+  return getMatrixGivenPitch(props)
 }
