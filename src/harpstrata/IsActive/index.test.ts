9325b16c13b0a03046848b0a6ece5cd5f41d7911
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-08 17:26:26 +0000

    Rephrase test description for clarity

--- src/harpstrata/IsActive/index.test.ts
+++ src/harpstrata/IsActive/index.test.ts
@@ -9,7 +9,7 @@ const { C_MAJOR_DIATONIC_FIRST_POZITION_C_MAJOR_PENTATONIC: {
 }} = EXAMPLE_STRATA
 const { activeDegreeIds, activePitchIds } = isActiveComplex
 
-test('getIsActiveComplex returns the IsActiveComplex for a c major diatonic harmonica with the c major pentatonic scale degrees active', () => {
+test('getIsActiveComplex returns the first pozition C major diatonic IsActiveComplex with C major pentatonic scale degrees active', () => {
   const isActiveProps = { degreeMatrix, pitchMatrix, activeIds: activeDegreeIds }
   const actualIsActiveComplex = getIsActiveComplex(isActiveProps)
   const expectedIsActiveComplex = isActiveComplex
@@ -17,7 +17,7 @@ test('getIsActiveComplex returns the IsActiveComplex for a c major diatonic harm
   expect(actualIsActiveComplex).toStrictEqual(expectedIsActiveComplex)
 })
 
-test('getIsActiveComplex returns the IsActiveComplex for a c major diatonic harmonica with the c major pentatonic scale pitches active', () => {
+test('getIsActiveComplex returns the first pozition C major diatonic IsActiveComplex with C major pentatonic scale pitches active', () => {
   const isActiveProps = { degreeMatrix, pitchMatrix, activeIds: activePitchIds }
   const actualIsActiveComplex = getIsActiveComplex(isActiveProps)
   const expectedIsActiveComplex = isActiveComplex
