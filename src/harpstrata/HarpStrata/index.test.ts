708ccfe55909ede01155859fc708de959941a668
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-08 22:40:54 +0000

    Organise HarpStrata module root index

--- src/harpstrata/HarpStrata/index.test.ts
+++ src/harpstrata/HarpStrata/index.test.ts
@@ -1,9 +1,8 @@
 import { PozitionIds } from '../Pozition'
 import { PitchIds } from '../Pitch'
-import { EXAMPLE_STRATA } from '../HarpStrata'
 import { ApparatusIds } from '../Apparatus'
 
-import { getApparatusIds, getPozitionIds, getPitchIds, getHarpStrata } from './index'
+import { getApparatusIds, getPozitionIds, getPitchIds } from './index'
 
 test('getApparatusIds function returns an array with a major diatonic as one of the values', () => {
   expect(getApparatusIds().includes(ApparatusIds.MajorDiatonic)).toBeTruthy()
@@ -15,44 +14,4 @@ test('getPozitionIds function returns an array with a first pozition as one of t
 
 test('getPitchIds function returns an array with a C as one of the values', () => {
   expect(getPitchIds().includes(PitchIds.C)).toBeTruthy()
-})
-
-test('getHarpStrata can return a first pozition C major diatonic HarpStrata with C major pentatonic IsActiveComplex given either set of ActiveIds', () => {
-  const { C_MAJOR_DIATONIC_FIRST_POZITION_C_MAJOR_PENTATONIC: expectedStrata } = EXAMPLE_STRATA
-  const { isActiveComplex: { activeDegreeIds,  activePitchIds }} = expectedStrata
-
-  const harpStrataPropsBase = {
-    apparatusId: ApparatusIds.MajorDiatonic,
-    pozitionId: PozitionIds.First,
-    keyPitchId: PitchIds.C,
-    activeIds: []
-  }
-  const harpStrataPropsWithActiveDegreeIds = { ...harpStrataPropsBase, activeIds: activeDegreeIds }
-  const harpStrataPropsWithActivePitchIds = { ...harpStrataPropsBase, activeIds: activePitchIds }
-
-  const strataFromDegreeIds = getHarpStrata(harpStrataPropsWithActiveDegreeIds)
-  const strataFromPitchIds = getHarpStrata(harpStrataPropsWithActivePitchIds)
-
-  expect(strataFromDegreeIds).toEqual(expectedStrata)
-  expect(strataFromPitchIds).toEqual(expectedStrata)
-})
-
-test('getHarpStrata can return a second pozition C major diatonic HarpStrata with G major pentatonic IsActiveComplex given either set of ActiveIds', () => {
-  const { C_MAJOR_DIATONIC_SECOND_POZITION_G_MAJOR_PENTATONIC: expectedStrata } = EXAMPLE_STRATA
-  const { isActiveComplex: { activeDegreeIds,  activePitchIds }} = expectedStrata
-
-  const harpStrataPropsBase = {
-    apparatusId: ApparatusIds.MajorDiatonic,
-    pozitionId: PozitionIds.Second,
-    keyPitchId: PitchIds.C,
-    activeIds: []
-  }
-  const harpStrataPropsWithActiveDegreeIds = { ...harpStrataPropsBase, activeIds: activeDegreeIds }
-  const harpStrataPropsWithActivePitchIds = { ...harpStrataPropsBase, activeIds: activePitchIds }
-
-  const strataFromDegreeIds = getHarpStrata(harpStrataPropsWithActiveDegreeIds)
-  const strataFromPitchIds = getHarpStrata(harpStrataPropsWithActivePitchIds)
-
-  expect(strataFromDegreeIds).toEqual(expectedStrata)
-  expect(strataFromPitchIds).toEqual(expectedStrata)
 })
--- src/harpstrata/HarpStrata/index.ts
+++ src/harpstrata/HarpStrata/index.ts
@@ -1,10 +1,6 @@
-import { getActivePozitionIds, getPozition } from '../Pozition'
-import { getPitchMatrix, getActivePitchIds, getPitch } from '../Pitch'
-import { getIsActiveComplex } from '../IsActive'
-import { getDegreeMatrix } from '../Degree'
-import { getActiveApparatusIds, getApparatus } from '../Apparatus'
-
-import type { HarpStrataProps, HarpStrata } from './types'
+import { getActivePozitionIds } from '../Pozition'
+import { getActivePitchIds } from '../Pitch'
+import { getActiveApparatusIds } from '../Apparatus'
 
 
 export const getApparatusIds = getActiveApparatusIds
@@ -13,28 +9,7 @@ export const getPozitionIds = getActivePozitionIds
 
 export const getPitchIds = getActivePitchIds
 
-export const getHarpStrata = (props: HarpStrataProps): HarpStrata => {
-  const { apparatusId, pozitionId, keyPitchId, activeIds } = props
-
-  const apparatus = getApparatus(apparatusId)
-  const pozition = getPozition(pozitionId)
-  const pitch = getPitch(keyPitchId)
-
-  const { halfstepIndexMatrix } = apparatus
-  const { root: pozitionRoot } = pozition
-  const { id: pitchId } = pitch
-
-  const degreeMatrix = getDegreeMatrix(halfstepIndexMatrix, pozitionRoot)
-  const pitchMatrix = getPitchMatrix(halfstepIndexMatrix, pitchId)
-  const isActiveComplex = getIsActiveComplex({degreeMatrix, pitchMatrix, activeIds: activeIds })
-
-  return {
-    apparatus,
-    degreeMatrix,
-    pitchMatrix,
-    isActiveComplex,
-  }
-}
+export { getHarpStrata } from './getHarpStrata'
 
 export { HarpStrataProps, HarpStrata } from './types'
 export { EXAMPLE_STRATA } from './testResources'
