79a57f4ad806eb5a58d4515bb962044da721ddfd
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-09 12:33:00 +0000

    Tidy exports

--- src/harpstrata/IsActive/IsActiveMatrix/index.ts
+++ src/harpstrata/IsActive/IsActiveMatrix/index.ts
@@ -1,4 +1,5 @@
 export { getIsActiveMatrix } from './getIsActiveMatrix'
-export type { IsActiveMatrix, IsActiveRow } from './types'
 export { IsActiveIds } from './types'
+export type { IsActiveMatrix, IsActiveRow } from './types'
+
 export { EXAMPLE_IS_ACTIVE_MATRICES } from './testResources'
