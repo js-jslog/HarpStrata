f3cf687ba410646a1f5fe6d74e0baf86e8b2c603
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-09 11:58:58 +0000

    Group exports for clarity

--- src/harpstrata/IsActive/index.ts
+++ src/harpstrata/IsActive/index.ts
@@ -1,6 +1,9 @@
 export { getIsActiveComplex } from './IsActiveComplex'
 export type { IsActiveComplex } from './IsActiveComplex'
+
 export type { ActiveDegreeIds, ActivePitchIds, ActiveIds } from './ActiveIds'
+
 export { IsActiveIds } from './IsActiveMatrix'
 export type { IsActiveMatrix, IsActiveRow } from './IsActiveMatrix'
+
 export { EXAMPLE_IS_ACTIVE_COMPLEXES } from './testResources'
