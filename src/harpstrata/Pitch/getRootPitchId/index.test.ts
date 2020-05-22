31db1646cbf55b774ca349687770b2ff1bf09ce7
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-22 22:39:28 +0000

    Add complex test of getRootPitchId functionality

--- src/harpstrata/Pitch/getRootPitchId/index.test.ts
+++ src/harpstrata/Pitch/getRootPitchId/index.test.ts
@@ -12,3 +12,23 @@ test('getRootPitchId returns C for a C harp in first pozition', () => {
 
   expect(actualRootPitchId).toBe(expectedRootPitchId)
 })
+
+test('getRootPitchId returns G for a C harp in second pozition', () => {
+  const { C: harpKeyId } = PitchIds
+  const { Second: pozitionId } = PozitionIds
+
+  const { G: expectedRootPitchId } = PitchIds
+  const actualRootPitchId = getRootPitchId({harpKeyId, pozitionId})
+
+  expect(actualRootPitchId).toBe(expectedRootPitchId)
+})
+
+test('getRootPitchId returns Bb for a Gb harp in fifth pozition', () => {
+  const { Gb: harpKeyId } = PitchIds
+  const { Fifth: pozitionId } = PozitionIds
+
+  const { Bb: expectedRootPitchId } = PitchIds
+  const actualRootPitchId = getRootPitchId({harpKeyId, pozitionId})
+
+  expect(actualRootPitchId).toBe(expectedRootPitchId)
+})
