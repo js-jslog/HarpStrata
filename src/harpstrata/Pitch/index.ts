b2f9142d74d2a02110619418b44033ae60062a8d
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-22 21:52:50 +0000

    Move pitchMap to dedicated folder

--- src/harpstrata/Pitch/index.ts
+++ src/harpstrata/Pitch/index.ts
@@ -1,19 +1,6 @@
-import type { PitchIds, Pitch } from './types'
-import { C, Db, D, Eb, E, F, Gb, G, Ab, A, Bb, B } from './constants'
+import { pitchMap } from './pitchMap'
 
-const pitchMap = new Map()
-pitchMap.set(C.id, C)
-pitchMap.set(Db.id, Db)
-pitchMap.set(D.id, D)
-pitchMap.set(Eb.id, Eb)
-pitchMap.set(E.id, E)
-pitchMap.set(F.id, F)
-pitchMap.set(Gb.id, Gb)
-pitchMap.set(G.id, G)
-pitchMap.set(Ab.id, Ab)
-pitchMap.set(A.id, A)
-pitchMap.set(Bb.id, Bb)
-pitchMap.set(B.id, B)
+import type { PitchIds, Pitch } from './types'
 
 export const getActivePitchIds = (): PitchIds[] => Array.from(pitchMap.keys())
 export const getPitch = (pitchId: PitchIds): Pitch => pitchMap.get(pitchId)
---
+++ src/harpstrata/Pitch/pitchMap/index.ts
@@ -0,0 +1,17 @@
+import { C, Db, D, Eb, E, F, Gb, G, Ab, A, Bb, B } from '../constants'
+
+const pitchMap = new Map()
+pitchMap.set(C.id, C)
+pitchMap.set(Db.id, Db)
+pitchMap.set(D.id, D)
+pitchMap.set(Eb.id, Eb)
+pitchMap.set(E.id, E)
+pitchMap.set(F.id, F)
+pitchMap.set(Gb.id, Gb)
+pitchMap.set(G.id, G)
+pitchMap.set(Ab.id, Ab)
+pitchMap.set(A.id, A)
+pitchMap.set(Bb.id, Bb)
+pitchMap.set(B.id, B)
+
+export { pitchMap }
