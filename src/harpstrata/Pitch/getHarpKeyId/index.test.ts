50c6e57b52add9fcd30eea9a6a6c1d8b191a906b
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-22 22:54:03 +0000

    Add tests to the harpkey discovery functionality

--- src/harpstrata/Pitch/getHarpKeyId/index.test.ts
+++ src/harpstrata/Pitch/getHarpKeyId/index.test.ts
@@ -12,3 +12,23 @@ test('getHarpKeyId returns C for a C root in first pozition', () => {
 
   expect(actualHarpKeyId).toBe(expectedHarpKeyId)
 })
+
+test('getHarpKeyId returns C for a G root in second pozition', () => {
+  const { G: rootPitchId } = PitchIds
+  const { Second: pozitionId } = PozitionIds
+
+  const { C: expectedHarpKeyId } = PitchIds
+  const actualHarpKeyId = getHarpKeyId({rootPitchId, pozitionId})
+
+  expect(actualHarpKeyId).toBe(expectedHarpKeyId)
+})
+
+test('getHarpKeyId returns D for an Eb root in eighth pozition', () => {
+  const { Eb: rootPitchId } = PitchIds
+  const { Eighth: pozitionId } = PozitionIds
+
+  const { D: expectedHarpKeyId } = PitchIds
+  const actualHarpKeyId = getHarpKeyId({rootPitchId, pozitionId})
+
+  expect(actualHarpKeyId).toBe(expectedHarpKeyId)
+})
