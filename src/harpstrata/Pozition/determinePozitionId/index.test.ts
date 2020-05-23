6823a02ec5ba304478db2d9100fa4176667524ac
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-23 11:45:25 +0000

    Add some more test to the pozition id determination

--- src/harpstrata/Pozition/determinePozitionId/index.test.ts
+++ src/harpstrata/Pozition/determinePozitionId/index.test.ts
@@ -12,3 +12,23 @@ test('determinePozitionId returns First for a C root on a C harp', () => {
 
   expect(actualPozitionId).toBe(expectedPozitionId)
 })
+
+test('determinePozitionId returns Second for a G root on a C harp', () => {
+  const { G: rootPitchId } = PitchIds
+  const { C: harpKeyId } = PitchIds
+
+  const { Second: expectedPozitionId } = PozitionIds
+  const actualPozitionId = determinePozitionId({ rootPitchId, harpKeyId })
+
+  expect(actualPozitionId).toBe(expectedPozitionId)
+})
+
+test('determinePozitionId returns Ninth for a Eb root on a G harp', () => {
+  const { Eb: rootPitchId } = PitchIds
+  const { G: harpKeyId } = PitchIds
+
+  const { Ninth: expectedPozitionId } = PozitionIds
+  const actualPozitionId = determinePozitionId({ rootPitchId, harpKeyId })
+
+  expect(actualPozitionId).toBe(expectedPozitionId)
+})
