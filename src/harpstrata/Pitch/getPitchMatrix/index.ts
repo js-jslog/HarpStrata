dd45aeabf9674c1c83aa0ab84b5227519374422a
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-23 22:21:36 +0000

    Use getOrderedPitchIds to replace ORDERED_PITCHES constant

--- src/harpstrata/Pitch/getPitchMatrix/index.ts
+++ src/harpstrata/Pitch/getPitchMatrix/index.ts
@@ -1,18 +1,15 @@
 import type { PitchIds, PitchMatrix } from '../types'
-import { ORDERED_PITCHES } from '../constants'
+import { getOrderedPitchIds, getPitch } from '../pitchMap'
 import type { HalfstepIndexMatrix } from '../../Apparatus'
 
 export const getPitchMatrix = (halfstepIndexMatrix: HalfstepIndexMatrix, keyPitchId: PitchIds): PitchMatrix => {
-  const orderedPitchIds = ORDERED_PITCHES.map(pitch => pitch.id)
-  const pitchIndex = orderedPitchIds.indexOf(keyPitchId)
-  const arrayHead = [ ...ORDERED_PITCHES.slice(pitchIndex) ]
-  const arrayTail = [ ...ORDERED_PITCHES.slice(0, (pitchIndex)) ]
-  const alignedPitchIds = [ ...arrayHead, ...arrayTail ]
+
+  const ascPitchIdsInKey = getOrderedPitchIds(keyPitchId)
 
   return halfstepIndexMatrix.map((halfstepIndexRow) => {
     return halfstepIndexRow.map((halfstepIndex) => {
       if (halfstepIndex === undefined) return undefined
-      return alignedPitchIds[halfstepIndex % 12]
+      return getPitch(ascPitchIdsInKey[halfstepIndex % 12])
     })
   })
 }
