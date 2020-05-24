410e47032d08b3a340593bba677cbd00e74d5e6c
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-24 19:17:36 +0000

    Make covariant tests more varied

--- src/harpstrata/Covariate/getCovariates/index.test.ts
+++ src/harpstrata/Covariate/getCovariates/index.test.ts
@@ -6,32 +6,32 @@ import { getCovariates } from './index'
 
 test('getCovariates returns the root pitch along with input harp key and pozition id control variables', () => {
   const { C: harpKeyId } = PitchIds
-  const { Second: pozitionId } = PozitionIds
+  const { Fourth: pozitionId } = PozitionIds
   const rootPitchControlProps: RootPitchControlVars = { harpKeyId, pozitionId }
 
-  const expectedCovariates = { harpKeyId, pozitionId, rootPitchId: PitchIds.G }
+  const expectedCovariates = { harpKeyId, pozitionId, rootPitchId: PitchIds.A }
   const actualCovariates = getCovariates(rootPitchControlProps)
 
   expect(actualCovariates).toEqual(expectedCovariates)
 })
 
 test('getCovariates returns the harp key along with input root pitch and pozition id control variables', () => {
-  const { G: rootPitchId } = PitchIds
-  const { Second: pozitionId } = PozitionIds
+  const { Bb: rootPitchId } = PitchIds
+  const { Ninth: pozitionId } = PozitionIds
   const harpKeyControlProps: HarpKeyControlVars = { rootPitchId, pozitionId }
 
-  const expectedCovariates = { rootPitchId, pozitionId, harpKeyId: PitchIds.C }
+  const expectedCovariates = { rootPitchId, pozitionId, harpKeyId: PitchIds.D }
   const actualCovariates = getCovariates(harpKeyControlProps)
 
   expect(actualCovariates).toEqual(expectedCovariates)
 })
 
 test('getCovariates returns the pozition along with input root pitch and harp key id control variables', () => {
-  const { G: rootPitchId } = PitchIds
+  const { F: rootPitchId } = PitchIds
   const { C: harpKeyId } = PitchIds
   const pozitionIdControlProps: PozitionControlVars = { rootPitchId, harpKeyId }
 
-  const expectedCovariates = { rootPitchId, pozitionId: PozitionIds.Second, harpKeyId }
+  const expectedCovariates = { rootPitchId, harpKeyId, pozitionId: PozitionIds.Twelfth }
   const actualCovariates = getCovariates(pozitionIdControlProps)
 
   expect(actualCovariates).toEqual(expectedCovariates)
