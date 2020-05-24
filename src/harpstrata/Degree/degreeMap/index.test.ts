f2ed4d72e79d34cd349fd04b1fa9fd888ee94f08
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-24 09:42:40 +0000

    Add degreeMap to replace ORDERED_DEGREES
    This was built in a single commit since it's just a copy of the
    pozitionMap and pitchMap patterns.

---
+++ src/harpstrata/Degree/degreeMap/index.test.ts
@@ -0,0 +1,34 @@
+import { DegreeIds } from '../types'
+import type { Degree } from '../types'
+
+import { getOrderedDegreeIds, getDegree } from './index'
+
+const { Root, Flat2, Second, Flat3, Third, Fourth, Flat5, Fifth, Flat6, Sixth, Flat7, Seventh } = DegreeIds
+
+
+test('getOrderedDegreeIds function returns an ordered array of the available pitches defaulting to starting at C', () => {
+  const expectedArray = [
+    Root, Flat2, Second, Flat3, Third, Fourth,
+    Flat5, Fifth, Flat6, Sixth, Flat7, Seventh
+  ]
+  const actualArray = getOrderedDegreeIds()
+
+  expect(actualArray).toEqual(expectedArray)
+})
+
+test('getOrderedDegreeIds function returns an ordered array of the available pitches starting from an input value', () => {
+  const expectedArray = [
+    Flat5, Fifth, Flat6, Sixth, Flat7, Seventh,
+    Root, Flat2, Second, Flat3, Third, Fourth
+  ]
+  const actualArray = getOrderedDegreeIds(DegreeIds.Flat5)
+
+  expect(actualArray).toEqual(expectedArray)
+})
+
+test('getDegree function can return a root degree', () => {
+  const ROOT_DEGREE: Degree = { id: DegreeIds.Root } as const
+  const actualDegree = getDegree(ROOT_DEGREE.id)
+
+  expect(actualDegree).toStrictEqual(ROOT_DEGREE)
+})
---
+++ src/harpstrata/Degree/degreeMap/index.ts
@@ -0,0 +1,30 @@
+import type { Degree } from '../types'
+import { DegreeIds } from '../types'
+import { ROOT, FLAT2, SECOND, FLAT3, THIRD, FOURTH, FLAT5, FIFTH, FLAT6, SIXTH, FLAT7, SEVENTH } from '../constants'
+
+const degreeMap = new Map()
+degreeMap.set(ROOT.id, ROOT)
+degreeMap.set(FLAT2.id, FLAT2)
+degreeMap.set(SECOND.id, SECOND)
+degreeMap.set(FLAT3.id, FLAT3)
+degreeMap.set(THIRD.id, THIRD)
+degreeMap.set(FOURTH.id, FOURTH)
+degreeMap.set(FLAT5.id, FLAT5)
+degreeMap.set(FIFTH.id, FIFTH)
+degreeMap.set(FLAT6.id, FLAT6)
+degreeMap.set(SIXTH.id, SIXTH)
+degreeMap.set(FLAT7.id, FLAT7)
+degreeMap.set(SEVENTH.id, SEVENTH)
+
+export const getOrderedDegreeIds = (zeroth: DegreeIds = DegreeIds.Root): ReadonlyArray<DegreeIds> => {
+  const orderedDegreeIds = Array.from(degreeMap.keys())
+
+  const desiredZeroIndex = orderedDegreeIds.indexOf(zeroth)
+
+  const head = [ ...orderedDegreeIds.slice(desiredZeroIndex) ]
+  const tail = [ ...orderedDegreeIds.slice(0, (desiredZeroIndex)) ]
+
+  return [ ...head, ...tail ]
+}
+
+export const getDegree = (degreeId: DegreeIds): Degree => degreeMap.get(degreeId)
