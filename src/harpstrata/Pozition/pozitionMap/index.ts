eb9349c55a52d7f742f2203f915eb10855680337
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-29 20:57:10 +0000

    Replace pozitionMap with enum reference and factory

--- src/harpstrata/Pozition/pozitionMap/index.ts
+++ src/harpstrata/Pozition/pozitionMap/index.ts
@@ -1,23 +1,8 @@
 import { PozitionIds } from '../types'
 import type { Pozition } from '../types'
-import { FIRST, SECOND, THIRD, FOURTH, FIFTH, SIXTH, SEVENTH, EIGHTH, NINTH, TENTH, ELEVENTH, TWELFTH } from '../constants'
-
-const pozitionMap = new Map()
-pozitionMap.set(FIRST.id, FIRST)
-pozitionMap.set(EIGHTH.id, EIGHTH)
-pozitionMap.set(THIRD.id, THIRD)
-pozitionMap.set(TENTH.id, TENTH)
-pozitionMap.set(FIFTH.id, FIFTH)
-pozitionMap.set(TWELFTH.id, TWELFTH)
-pozitionMap.set(SEVENTH.id, SEVENTH)
-pozitionMap.set(SECOND.id, SECOND)
-pozitionMap.set(NINTH.id, NINTH)
-pozitionMap.set(FOURTH.id, FOURTH)
-pozitionMap.set(ELEVENTH.id, ELEVENTH)
-pozitionMap.set(SIXTH.id, SIXTH)
 
 export const getAscendingPozitionIds = (originId: PozitionIds = PozitionIds.First): ReadonlyArray<PozitionIds> => {
-  const ascendingPozitionIds = Array.from(pozitionMap.keys())
+  const ascendingPozitionIds = Object.values(PozitionIds)
 
   const originIndex = ascendingPozitionIds.indexOf(originId)
 
@@ -29,4 +14,4 @@ export const getAscendingPozitionIds = (originId: PozitionIds = PozitionIds.Firs
 
 export const getPozitionRootOffset = (pozitionId: PozitionIds): number => getAscendingPozitionIds().indexOf(pozitionId)
 
-export const getPozition = (pozitionId: PozitionIds): Pozition => pozitionMap.get(pozitionId)
+export const getPozition = (pozitionId: PozitionIds): Pozition => ({ id: pozitionId })
