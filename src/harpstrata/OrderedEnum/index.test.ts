95045adb4b35ff0726035be551d9f4417174d96d
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-31 18:39:19 +0000

    Test that another enum can be run through function

--- src/harpstrata/OrderedEnum/index.test.ts
+++ src/harpstrata/OrderedEnum/index.test.ts
@@ -1,8 +1,14 @@
+import { PitchIds } from '../Pitch'
 import { DegreeIds } from '../Degree'
 
 import { getOrderedEnum } from './index'
 
-test('returns the values from the Degree enum', () => {
+test('returns the values from the DegreeIds', () => {
   const expectedArray = Object.values(DegreeIds)
   expect(getOrderedEnum(DegreeIds)).toStrictEqual(expectedArray)
+})
+
+test('returns the values from the PitchIds', () => {
+  const expectedArray = Object.values(PitchIds)
+  expect(getOrderedEnum(PitchIds)).toStrictEqual(expectedArray)
 })
