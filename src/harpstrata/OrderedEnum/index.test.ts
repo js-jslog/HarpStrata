f090953c46469da95ccf84a3a848d43adc478a80
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-31 18:40:29 +0000

    Improve test description

--- src/harpstrata/OrderedEnum/index.test.ts
+++ src/harpstrata/OrderedEnum/index.test.ts
@@ -3,12 +3,12 @@ import { DegreeIds } from '../Degree'
 
 import { getOrderedEnum } from './index'
 
-test('returns the values from the DegreeIds', () => {
+test('returns the values from the DegreeIds enum', () => {
   const expectedArray = Object.values(DegreeIds)
   expect(getOrderedEnum(DegreeIds)).toStrictEqual(expectedArray)
 })
 
-test('returns the values from the PitchIds', () => {
+test('returns the values from the PitchIds enum', () => {
   const expectedArray = Object.values(PitchIds)
   expect(getOrderedEnum(PitchIds)).toStrictEqual(expectedArray)
 })
