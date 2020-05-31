d421b38d60f35d09eb4129b00fbadc0d7f1c430e
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-31 20:19:02 +0000

    Replace degree ids source in degree matrix

--- src/harpstrata/Degree/getDegreeMatrix/index.ts
+++ src/harpstrata/Degree/getDegreeMatrix/index.ts
@@ -1,19 +1,12 @@
 import type { DegreeMatrix } from '../types'
 import { getDegree } from '../getDegree'
-import { Orderables, getAscendingIds, getDescendingIds } from '../../OrderedIds'
-import type { OrderedDegreeIdsProps } from '../../OrderedIds'
-import type { DegreeIds } from '../../Degree'
+import { getAscendingDegreeIds, getDescendingDegreeIds } from '../../OrderedEnum'
 import type { HalfstepIndexMatrix, HalfstepIndex } from '../../Apparatus'
 
 
 export const getDegreeMatrix = (halfstepIndexMatrix: HalfstepIndexMatrix, root: HalfstepIndex): DegreeMatrix => {
-  const orderedDegreeIdsProps: OrderedDegreeIdsProps = { type: Orderables.Degree }
-  const { [root]: rootDegreeId } = getDescendingIds(orderedDegreeIdsProps) as ReadonlyArray<DegreeIds>
-  const orderedDegreeIdsPropsFromOrigin: OrderedDegreeIdsProps = {
-    ...orderedDegreeIdsProps,
-    origin: rootDegreeId,
-  }
-  const ascendingDegreeIdsFromHarpRoot = getAscendingIds(orderedDegreeIdsPropsFromOrigin) as ReadonlyArray<DegreeIds>
+  const { [root]: rootDegreeId } = getDescendingDegreeIds()
+  const ascendingDegreeIdsFromHarpRoot = getAscendingDegreeIds(rootDegreeId)
 
   return halfstepIndexMatrix.map((halfstepIndexRow) => {
     return halfstepIndexRow.map((halfstepIndex) => {
--- src/harpstrata/OrderedEnum/index.ts
+++ src/harpstrata/OrderedEnum/index.ts
@@ -2,10 +2,10 @@ import { DegreeIds } from '../Degree'
 
 import { getAscendingEnumValues, getDescendingEnumValues } from './OrderedEnumValues'
 
-export const getAscendingDegreeIds = (origin: DegreeIds): ReadonlyArray<DegreeIds> => {
+export const getAscendingDegreeIds = (origin?: DegreeIds): ReadonlyArray<DegreeIds> => {
   return getAscendingEnumValues(DegreeIds, origin)
 }
 
-export const getDescendingDegreeIds = (origin: DegreeIds): ReadonlyArray<DegreeIds> => {
+export const getDescendingDegreeIds = (origin?: DegreeIds): ReadonlyArray<DegreeIds> => {
   return getDescendingEnumValues(DegreeIds, origin)
 }
