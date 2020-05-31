e418ca523ad54bc1e64b5cc8b21e6a286baf8c96
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-31 20:06:46 +0000

    Add degree specialised descending ids fucntion

--- src/harpstrata/OrderedEnum/index.test.ts
+++ src/harpstrata/OrderedEnum/index.test.ts
@@ -1,6 +1,6 @@
 import { DegreeIds } from '../Degree'
 
-import { getAscendingDegreeIds } from './index'
+import { getAscendingDegreeIds, getDescendingDegreeIds } from './index'
 
 
 test('Degree specific function returns the values from the DegreeIds enum from the parameterised origin', () => {
@@ -13,4 +13,16 @@ test('Degree specific function returns the values from the DegreeIds enum from t
     Root, Flat2, Second, Flat3, Third, Fourth
   ]
   expect(getAscendingDegreeIds(Flat5)).toStrictEqual(expectedArray)
+})
+
+test('Degree specific function returns the descending values from the DegreeIds enum from the parameterised origin', () => {
+  const {
+    Root, Flat2, Second, Flat3, Third, Fourth,
+    Flat5, Fifth, Flat6, Sixth, Flat7, Seventh,
+  } = DegreeIds
+  const expectedArray = [
+    Flat5, Fourth, Third, Flat3, Second, Flat2,
+    Root, Seventh, Flat7, Sixth, Flat6, Fifth
+  ]
+  expect(getDescendingDegreeIds(Flat5)).toStrictEqual(expectedArray)
 })
--- src/harpstrata/OrderedEnum/index.ts
+++ src/harpstrata/OrderedEnum/index.ts
@@ -1,7 +1,11 @@
 import { DegreeIds } from '../Degree'
 
-import { getAscendingEnumValues } from './OrderedEnumValues'
+import { getAscendingEnumValues, getDescendingEnumValues } from './OrderedEnumValues'
 
 export const getAscendingDegreeIds = (origin: DegreeIds): ReadonlyArray<DegreeIds> => {
   return getAscendingEnumValues(DegreeIds, origin)
+}
+
+export const getDescendingDegreeIds = (origin: DegreeIds): ReadonlyArray<DegreeIds> => {
+  return getDescendingEnumValues(DegreeIds, origin)
 }
