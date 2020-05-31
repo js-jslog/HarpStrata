880e58ffa547f8b72cf0c83bcc913e92d67f3910
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-31 19:00:05 +0000

    Allow ordering to begin at defined origin

--- src/harpstrata/OrderedEnum/index.test.ts
+++ src/harpstrata/OrderedEnum/index.test.ts
@@ -3,12 +3,34 @@ import { DegreeIds } from '../Degree'
 
 import { getOrderedEnum } from './index'
 
-test('returns the values from the DegreeIds enum', () => {
-  const expectedArray = Object.values(DegreeIds)
+const {
+  Root, Flat2, Second, Flat3,
+  Third, Fourth, Flat5, Fifth,
+  Flat6, Sixth, Flat7, Seventh
+} = DegreeIds
+
+const {
+  C, Db, D, Eb, E, F,
+  Gb, G, Ab, A, Bb, B
+} = PitchIds
+
+test('returns the values from the DegreeIds enum defaulting to Root', () => {
+  const expectedArray = [
+    Root, Flat2, Second, Flat3, Third, Fourth,
+    Flat5, Fifth, Flat6, Sixth, Flat7, Seventh,
+  ]
   expect(getOrderedEnum(DegreeIds)).toStrictEqual(expectedArray)
 })
 
+test('returns the values from the DegreeIds enum from the parameterised origin', () => {
+  const expectedArray = [
+    Flat5, Fifth, Flat6, Sixth, Flat7, Seventh,
+    Root, Flat2, Second, Flat3, Third, Fourth
+  ]
+  expect(getOrderedEnum(DegreeIds, Flat5)).toStrictEqual(expectedArray)
+})
+
 test('returns the values from the PitchIds enum', () => {
-  const expectedArray = Object.values(PitchIds)
+  const expectedArray = [ A, Bb, B, C, Db, D, Eb, E, F, Gb, G, Ab ]
   expect(getOrderedEnum(PitchIds)).toStrictEqual(expectedArray)
 })
--- src/harpstrata/OrderedEnum/index.ts
+++ src/harpstrata/OrderedEnum/index.ts
@@ -1,3 +1,9 @@
-export const getOrderedEnum = <T>(enumerator: Record<string, T>): ReadonlyArray<T> => {
-  return Object.values(enumerator)
+export const getOrderedEnum = <T>(enumerator: Record<string, T>, origin: T = Object.values(enumerator)[0]): ReadonlyArray<T> => {
+  const ascendingValues = Object.values(enumerator)
+  const originIndex = ascendingValues.indexOf(origin)
+
+  const head = [ ...ascendingValues.slice(originIndex) ]
+  const tail = [ ...ascendingValues.slice(0, (originIndex)) ]
+
+  return [ ...head, ...tail ]
 }
