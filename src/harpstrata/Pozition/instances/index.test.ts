2187eb65bb4a70972a9b5601d11a2540e12c24d9
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-04 06:48:02 +0000

    Test instanceMap has all (and only) required keys

--- src/harpstrata/Pozition/instances/index.test.ts
+++ src/harpstrata/Pozition/instances/index.test.ts
@@ -1,3 +1,5 @@
+import { PozitionIds } from '../types'
+
 import { instanceMap } from './index'
 
 test('instanceMap is exported', () => {
@@ -6,4 +8,19 @@ test('instanceMap is exported', () => {
 
 test('Pozition instanceMap has exactly 12 mappings', () => {
   expect(instanceMap.size).toBe(12)
+})
+
+test('Pozition instanceMap has a mapping for each of the PozitionIds', () => {
+  expect(instanceMap.get(PozitionIds.First)).toBeTruthy()
+  expect(instanceMap.get(PozitionIds.Second)).toBeTruthy()
+  expect(instanceMap.get(PozitionIds.Third)).toBeTruthy()
+  expect(instanceMap.get(PozitionIds.Fourth)).toBeTruthy()
+  expect(instanceMap.get(PozitionIds.Fifth)).toBeTruthy()
+  expect(instanceMap.get(PozitionIds.Sixth)).toBeTruthy()
+  expect(instanceMap.get(PozitionIds.Seventh)).toBeTruthy()
+  expect(instanceMap.get(PozitionIds.Eighth)).toBeTruthy()
+  expect(instanceMap.get(PozitionIds.Ninth)).toBeTruthy()
+  expect(instanceMap.get(PozitionIds.Tenth)).toBeTruthy()
+  expect(instanceMap.get(PozitionIds.Eleventh)).toBeTruthy()
+  expect(instanceMap.get(PozitionIds.Twelfth)).toBeTruthy()
 })
