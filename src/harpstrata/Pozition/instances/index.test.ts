1dee52fbb56b0ba69a7ad006108bbaa3ea5fd561
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-04 06:44:44 +0000

    Make instanceMap the right size

--- src/harpstrata/Pozition/instances/index.test.ts
+++ src/harpstrata/Pozition/instances/index.test.ts
@@ -3,3 +3,7 @@ import { instanceMap } from './index'
 test('instanceMap is exported', () => {
   expect(instanceMap).toBeTruthy()
 })
+
+test('Pozition instanceMap has exactly 12 mappings', () => {
+  expect(instanceMap.size).toBe(12)
+})
--- src/harpstrata/Pozition/instances/index.ts
+++ src/harpstrata/Pozition/instances/index.ts
@@ -1,1 +1,16 @@
+import { PozitionIds } from '../types'
+
 export const instanceMap = new Map()
+
+instanceMap.set(PozitionIds.First, {})
+instanceMap.set(PozitionIds.Second, {})
+instanceMap.set(PozitionIds.Third, {})
+instanceMap.set(PozitionIds.Fourth, {})
+instanceMap.set(PozitionIds.Fifth, {})
+instanceMap.set(PozitionIds.Sixth, {})
+instanceMap.set(PozitionIds.Seventh, {})
+instanceMap.set(PozitionIds.Eighth, {})
+instanceMap.set(PozitionIds.Ninth, {})
+instanceMap.set(PozitionIds.Tenth, {})
+instanceMap.set(PozitionIds.Eleventh, {})
+instanceMap.set(PozitionIds.Twelfth, {})
