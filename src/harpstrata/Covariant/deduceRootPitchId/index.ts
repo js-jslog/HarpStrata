fea27607225499187d65c280d1ac188f153beeb0
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-04 13:46:15 +0000

    Remove getPozitionRootOffset
    It's no longer required.

--- src/harpstrata/Covariant/deduceRootPitchId/index.ts
+++ src/harpstrata/Covariant/deduceRootPitchId/index.ts
@@ -1,12 +1,12 @@
 import { RootPitchControlVars } from '../types'
-import { getPozitionRootOffset } from '../../Pozition'
+import { getPozition } from '../../Pozition'
 import type { PitchIds } from '../../Pitch'
 import { getAscendingPitchIds } from '../../OrderedIds'
 
 
 export const deduceRootPitchId = (props: RootPitchControlVars): PitchIds => {
   const { harpKeyId, pozitionId } = props
-  const rootOffset = getPozitionRootOffset(pozitionId)
+  const { rootOffset } = getPozition(pozitionId)
 
   const ascendingPitchIds = getAscendingPitchIds(harpKeyId)
 
