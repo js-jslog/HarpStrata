8ea97781008523ddd581f29aa30ac3ba972ef26c
js-jslog <jhs4jbs@hotmail.co.uk>
2020-08-20 14:14:35 +0000

    Add contextual information to Pitch objects
    There is a lot of depth to the question of how a given pitch is
    represented. Sometimes you may even want to represent a C as a B#. That
    kind of depth is not captured by this implementation. This
    implementation basically just observes that black notes have a modifier
    on them and which modifier that is depends on which foundation note you
    are modifying from.
    
    That seems to be sufficient for a representing notes on a harmonica
    face. If the requirements become more intense then this can change.

--- src/harpstrata/Pitch/types/index.ts
+++ src/harpstrata/Pitch/types/index.ts
@@ -13,8 +13,29 @@ export enum PitchIds {
   Ab = 'Ab',
 }
 
+export enum NoteFoundations {
+  A = 'A',
+  B = 'B',
+  C = 'C',
+  D = 'D',
+  E = 'E',
+  F = 'F',
+  G = 'G',
+}
+
+type NaturalDisplayValue = {
+  natural: NoteFoundations
+}
+type UnnaturalDisplayValue = {
+  sharp: NoteFoundations
+  flat: NoteFoundations
+}
+export type ContextualDisplayValue = NaturalDisplayValue | UnnaturalDisplayValue
+
+
 export type Pitch = {
   readonly id: PitchIds;
+  readonly contextualDisplayValue: ContextualDisplayValue
 }
 
 export type PitchRow = ReadonlyArray<Pitch | undefined>
